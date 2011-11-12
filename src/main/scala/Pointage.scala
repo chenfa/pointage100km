37145c992f42fcfcf41d8b089864260b89d33475
Tomo <thomas.gambier@gmail.com>
2011-11-12 13:59:38 +0100

    First skeleton of what I want to do

--- src/main/scala/Pointage.scala
+++ src/main/scala/Pointage.scala
@@ -9,85 +9,56 @@ import akka.actor.{ Actor, ActorRef, PoisonPill }
 import Actor._
 
 /**
- * For now we just do a simple application reading from different Unix fifos
- * and writing the input in a text file
+ * For now we just read from stdin some numbers
+ * We send the input string to a new worker that will:
+ *   - check that the string is a number
+ *   - write the string to a text file
+ *   - try to send the message to a server
  */
 object Pointage extends App {
-  if (args.length == 0) {
-    println("Please give files to parse at command line!")
-    println("Usage: Pointage fifo1 fifo2 ..... fifoN")
-  } else
-    go(args)
+  println("===== Bienvenue dans l'application de pointage =====\n")
+  println("Veuillez entrer les numéros de dossard l'un après l'autre")
 
-  def go(args: Array[String]) = {
-    var start = System.currentTimeMillis
-    println("=========================")
+  val master = actorOf(new Master())
 
-    val tasks =
-      for {
-        file <- args
-      } yield Task(file)
+  println("Starting the master and the monitor....")
+  master.start()
 
-    val master = actorOf(new Master(tasks.length)).start()
-
-    println("Waiting for the master response....")
-    val response = master !! tasks
-
-    println("====The following numbers has been found====")
-
-    response match {
-      case Some(numbers) =>
-        numbers match {
-          case numbersAsIter:Iterable[Any] =>
-            println(numbersAsIter mkString("\n"))
-          case _ @response =>
-            println("Unknown response format : " + response)
-        }
-      case None => println("No phone number found")
-    }
-
-    println("Total time : " + (System.currentTimeMillis - start) + " ms")
-    println("=========================")
+  stdin.getLines().foreach {
+    line => line match {
+      case "EOF" =>
+        println ("Will exit")
+        master.stop()
+        System.exit(0)
+      case the_line =>
+        println ("Have read : " + the_line)
+        val input = new NewInput(the_line)
+        master ! input
+        println ("and sent it to master.")
+      }
   }
 
+
   abstract class Message
-  case class Task(file: String) extends Message
-  case class Result(numbers: Set[String]) extends Message
+  case class NewInput(input: String) extends Message // From main application to worker
+  case class NewBib(bib: Integer) extends Message // From main application to worker
+  case class Problem(pb: String) extends Message // From worker to monitorStdin
 
-  class Master(nbOfWorkers: Int) extends Actor {
-    var responseCount = 0
-    var senderFuture: Option[CompletableFuture[Any]] = _
-    var phoneNumbers = Set[String]()
-    //We create and start worker actors.
-    //An actor for parsing each file content
-    val workers = Vector.fill(nbOfWorkers)(actorOf[Worker].start())
-
+  class Master() extends Actor {
+    var workers : List[ActorRef] = Nil // list of all running worker
     //We create the load balancer with a round round robin strategy
-    val loadBalancer = Routing.loadBalancerActor(CyclicIterator(workers))
+    //val loadBalancer = Routing.loadBalancerActor(CyclicIterator(workers))
 
     def receive = {
-      case Result(intermediateNumbers) =>
-        phoneNumbers = phoneNumbers ++ intermediateNumbers
-        responseCount += 1
-
-        if (responseCount == nbOfWorkers) {
-          senderFuture.foreach(_.completeWithResult(phoneNumbers))
-          loadBalancer ! Broadcast(PoisonPill)
-          loadBalancer ! PoisonPill
-          self.stop()
-        }
-
-      case tasks: Array[Task] =>
-        println("Received task list size = " + tasks.length)
-        //tasks foreach { println(_) }
-        senderFuture = self.senderFuture
-        senderFuture foreach {_.onTimeout((x: Future[Any]) => println("TIMEOUTTTTTTTT"))}
-        senderFuture foreach {(x: Future[Any]) => println("Timeout is " + x.timeoutInNanos)} // 60 seconds
-
-        for (task <- tasks) {
-          loadBalancer ! task
-          Work.taskCount += 1
-        }
+      case NewInput(input) =>
+        println("Master received a new input: " + input)
+        // check that input is an Integer
+        // print in text file
+        // send message to worker
+      case Problem(pb) =>
+        println("Worker had a problem: " + pb)
+      case _ =>
+        println("Master received a message")
     }
 
     override def preStart() = {
@@ -104,20 +75,15 @@ object Pointage extends App {
   }
 
   /**
-   * A worker read a fifo until it reads EOF.
+   * A worker just send an AMQP message to the server.
    */
   class Worker extends Actor {
-    val phoneRegex = "0[1-9]([ .-]?[0-9]{2}){4}".r
-
     def receive = {
-      case Task(file) =>
-        println("Worker doing hard work on file " + file)
-        var phoneNumbers = Set[String]()
-        for(line <- fromFile(file).mkString) {
-          println (file + ": " + line)
-          //phoneNumbers += phoneNumber
-        }
-        self reply Result(phoneNumbers)
+      case NewBib(bib) =>
+        println("Worker doing hard work on bib " + bib)
+        println("Here we should send a message to the server with AMQP")
+        println("Sleeping for a moment instead :)")
+        Thread.sleep(5000)
     }
 
     override def preStart() = {
